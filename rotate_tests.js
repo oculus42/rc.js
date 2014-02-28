de647fb1e695d60e2ee4f276788de011f29738ed
Samuel Rouse <samuel.rouse@gmail.com>
2014-02-28 07:44:48 -0500

    Rotate Tests 5
    Fixed array length. Mixed results:
    http://jsperf.com/rc-rotate-tests/5

--- rotate_tests.js
+++ rotate_tests.js
@@ -1,5 +1,5 @@
-// http://jsperf.com/rc-rotate-tests/2
-var rowData = [],i, j, ob;
+// http://jsperf.com/rc-rotate-tests/5
+var rowData = [], rowData2 = [], i, j, ob;
 
 for (i=0; i < 1e3; i++) {
 	ob = {
@@ -8,154 +8,18 @@ for (i=0; i < 1e3; i++) {
 		value: Math.floor(Math.random() * 1e4) / 100
 	}
 	rowData.push(ob);
+	
+	ob2 = {
+		id: Math.floor(Math.random() * 1e7),
+		age: Math.floor(Math.random() * 100),
+		value: Math.floor(Math.random() * 1e4) / 100
+	}
+	rowData2.push(ob2);
 }
 
 var Opt = Object.prototype.toString,
-	rotate_base =  function(arr, result, limited, softFail) {
-		var obj, len, i, att, has;
-	
-		// Not an array? Send it back.
-		if ( Opt.call(arr) !== '[object Array]' ) {
-			throw new TypeError("RC: Argument is not an array");
-		}
-	
-		// Clean truthy/falsy values & undefined
-		limited = !!limited;
-		softFail = !!softFail;
-	
-		if ( !!result || typeof result !== 'object' ) {
-			if ( limited ) {
-				throw new TypeError("RC: Must pass a result object when 'limited' is true");
-			} else {
-				result = {};
-			}
-		}
-	
-		len = arr.length;
-	
-		for ( i = 0; i < len; i++ ) {
-			obj = arr[i];
-	
-			if (typeof obj !== "object") {
-				if ( !softFail ) {
-					throw new TypeError("RC: Nested element is not an object");
-				}
-				// Otherwise skip this loop
-			} else {
-				for (att in obj) {
-					if ( obj.hasOwnProperty(att) ) {
-						has = result.hasOwnProperty(att);
-	
-						if ( !has ) {
-							if ( limited ) {
-								continue;
-							} else {
-								result[att] = [];
-							}
-						}
-						result[att][i] = obj[att];
-					}
-				}
-			}
-		}
-		return result;
-	},
-	rotate_backwards =  function(arr, result, limited, softFail) {
-		var obj, i, att, has;
-	
-		// Not an array? Send it back.
-		if ( Opt.call(arr) !== '[object Array]' ) {
-			throw new TypeError("RC: Argument is not an array");
-		}
-	
-		// Clean truthy/falsy values & undefined
-		limited = !!limited;
-		softFail = !!softFail;
-	
-		if ( !!result || typeof result !== 'object' ) {
-			if ( limited ) {
-				throw new TypeError("RC: Must pass a result object when 'limited' is true");
-			} else {
-				result = {};
-			}
-		}
-	
-		for ( i = arr.length; i; ) {
-			i--;
-			obj = arr[i];
-	
-			if (typeof obj !== "object") {
-				if ( !softFail ) {
-					throw new TypeError("RC: Nested element is not an object");
-				}
-				// Otherwise skip this loop
-			} else {
-				for (att in obj) {
-					if ( obj.hasOwnProperty(att) ) {
-						if ( !result.hasOwnProperty(att) ) {
-							if ( limited ) {
-								continue;
-							} else {
-								result[att] = [];
-							}
-						}
-						result[att][i] = obj[att];
-					}
-				}
-			}
-		}
-		return result;
-	},
-	
-	rotate_undef =  function(arr, result, limited, softFail) {
-		var obj, i, att, has;
-	
-		// Not an array? Send it back.
-		if ( Opt.call(arr) !== '[object Array]' ) {
-			throw new TypeError("RC: Argument is not an array");
-		}
-	
-		// Clean truthy/falsy values & undefined
-		limited = !!limited;
-		softFail = !!softFail;
-	
-		if ( !!result || typeof result !== 'object' ) {
-			if ( limited ) {
-				throw new TypeError("RC: Must pass a result object when 'limited' is true");
-			} else {
-				result = {};
-			}
-		}
-	
-		for ( i = arr.length; i; ) {
-			i--;
-			obj = arr[i];
-	
-			if (typeof obj !== "object") {
-				if ( !softFail ) {
-					throw new TypeError("RC: Nested element is not an object");
-				}
-				// Otherwise skip this loop
-			} else {
-				for (att in obj) {
-					if ( obj.hasOwnProperty(att) ) {
-						if ( result[att] === undefined ) {
-							if ( limited ) {
-								continue;
-							} else {
-								result[att] = [];
-							}
-						}
-						result[att][i] = obj[att];
-					}
-				}
-			}
-		}
-		return result;
-	},
-	
-	rotate_obj =  function(arr, result, limited, softFail) {
-		var obj, i, att, has;
+	rotate_v05 =  function(arr, result, limited, softFail) {
+		var obj, i, att;
 	
 		// Not an array? Send it back.
 		if ( Opt.call(arr) !== '[object Array]' ) {
@@ -186,6 +50,49 @@ var Opt = Object.prototype.toString,
 							continue;
 						} else {
 							result[att] = [];
+						}
+					}
+					result[att][i] = obj[att];
+				}
+			}
+		}
+		return result;
+	},
+	
+	rotate_fixed =  function(arr, result, limited, softFail) {
+		var len, obj, i, att;
+		
+		// Not an array? Send it back.
+		if ( Opt.call(arr) !== '[object Array]' ) {
+			throw new TypeError("RC: Argument is not an array");
+		}
+		
+		len = arr.length;
+	
+		// Clean truthy/falsy values & undefined
+		limited = !!limited;
+		softFail = !!softFail;
+	
+		if ( !!result || typeof result !== 'object' ) {
+			if ( limited ) {
+				throw new TypeError("RC: Must pass a result object when 'limited' is true");
+			} else {
+				result = {};
+			}
+		}
+	
+		for ( i = len; i; ) {
+			i--;
+			obj = arr[i];
+	
+			// All variables submit to for-in loops
+			for (att in obj) {
+				if ( obj.hasOwnProperty(att) ) {
+					if ( result[att] === undefined ) {
+						if ( limited ) {
+							continue;
+						} else {
+							result[att] = new Array(len);
 						}
 					}
 					result[att][i] = obj[att];
