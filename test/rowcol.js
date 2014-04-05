a6a83dc222bd0088657de92d8d91c0fbaa33b9ba
Samuel Rouse <samuel.rouse@gmail.com>
2014-04-05 11:44:26 -0400

    Fix filter

--- test/rowcol.js
+++ test/rowcol.js
@@ -1,10 +1,10 @@
 var assert = require("assert");
-var rowcol = require("rowcol");
+var rowcol = require("../rowcol");
 
 var rowData = [
 	{ id: 1, name: "A", approved: true},
 	{ id: 2, name: "B", approved: true},
-	{ id: 3, name: "C", approved: false},
+	{ id: 3, name: "C", approved: false}
 ],
 	colData = {
 		id: [1,2,3],
@@ -15,7 +15,38 @@ var rowData = [
 /* Explicit Rotates */
 assert.equal(JSON.stringify(rowcol.array.rotate(rowData)), JSON.stringify(colData), "row->col rotation - explicit");
 assert.equal(JSON.stringify(rowcol.object.rotate(colData)), JSON.stringify(rowData), "col->row rotation - explicit");
+
 /* Generic rotates (should work as above) */
 assert.equal(JSON.stringify(rowcol.rotate(rowData)), JSON.stringify(colData), "row->col rotation - generic");
 assert.equal(JSON.stringify(rowcol.rotate(colData)), JSON.stringify(rowData), "col->row rotation - generic");
 
+/* Filters */
+var filtTest = rowcol.object.filterIndexes(colData, "approved", true)
+assert(filtTest.length === 2 && filtTest[0] === 0 && filtTest[1] === 1, "filterIndexes: basic match");
+
+filtTest = rowcol.object.filterIndexes(colData, "approved", false)
+assert(filtTest.length === 1 && filtTest[0] === 2, "filterIndexes: basic match part 2");
+
+/* ObjFromIndex */
+var idxTest = rowcol.object.objFromIndex(colData, 1);
+assert.equal(idxTest.name,colData.name[1],"objFromIndex");
+
+idxTest.name = "M";
+assert.equal(colData.name[1], "B", "objFromIndex: value linked to original");
+
+assert(idxTest.hasOwnProperty("commit") === false, "objFromIndex: provides proxy interface");
+
+/* Proxy Test */
+var prox = rowcol.proxy(colData, 1);
+
+prox.approved = false;
+assert.equal(colData.approved[1], true, "proxy: commit prior value");
+
+prox.commit();
+assert.equal(colData.approved[1], false, "proxy: commit value mismatch");
+
+prox.name = "D";
+assert.equal(colData.name[1], "B", "proxy: finalize prior value");
+
+prox.finalize();
+assert.equal(colData.name[1], "D", "proxy: finalize value mismatch");
