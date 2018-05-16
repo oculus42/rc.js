00da27fbad0e42059065c123736ce9e2905670b6
Samuel Rouse <samuel.rouse@gmail.com>
2018-05-16 05:17:17 -0400

    CodeClimate cleanup

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function (grunt) {
+module.exports = (grunt) => {
 //   const banner = `/*
 // <%= pkg.name %> <%= pkg.version %>- <%= pkg.description %>
 // <%= pkg.repository.url %>
